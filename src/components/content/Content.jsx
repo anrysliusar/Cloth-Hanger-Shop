47baa101db98c3e6c7c26d9fcafe5db4f9913f54
andri <assliusarenko@gmail.com>
2021-01-04 14:34:22 +0200

    add content block

---
+++ src/components/content/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Content.module.css'
+
+const Content = () =>{
+    return <div className={classes.content}>
+        <h3 className={classes.title}>Content</h3>
+    </div>
+}
+
+export default Content
