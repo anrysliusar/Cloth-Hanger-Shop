2a769ec379634d52e9c6235a75ec9cb422f03348
andri <assliusarenko@gmail.com>
2021-01-04 14:35:53 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import Header from "./components/header/Header";
+import classes from './styles.module.css'
+import NavbarGender from "./components/navbars/navbarGender/NavbarGender";
+import NavbarCategory from "./components/navbars/navbarCategory/NavbarCategory";
+import Sidebar from "./components/sidebar/Sidebar";
+import Content from "./components/content/Content";
 
 class App extends Component {
     render() {
         return (
-            <div>
-              <Header/>
+            <div className={classes.wrapper}>
+                <Header/>
+                {/*<NavbarGender/>*/}
+                <NavbarCategory/>
+                <div className={classes.mainBlock}>
+                    <Sidebar/>
+                    <Content/>
+                </div>
+
             </div>
         );
     }
