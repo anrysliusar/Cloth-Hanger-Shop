1cdd740a87fa3b695d6d0b4b08f86e82c88c3fc5
andri <assliusarenko@gmail.com>
2021-01-05 18:39:27 +0200

    update files

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Content.module.css'
 import {Route, Switch} from "react-router-dom";
-import Coat from "../categories/coats/Coat";
+import AllCoats from "../categories/coats/AllCoats";
 import Shirt from "../categories/shirts/Shirt";
 import Shoe from "../categories/shoes/Shoe";
 import Jeans from "../categories/jeans/Jeans";
@@ -9,7 +9,7 @@ import Jeans from "../categories/jeans/Jeans";
 const Content = () =>{
     return <div className={classes.content}>
         <Switch>
-            <Route path={'/coats'} component={Coat}/>
+            <Route path={'/coats'} component={AllCoats}/>
             <Route path={'/shirts'} component={Shirt}/>
             <Route path={'/shoes'} component={Shoe}/>
             <Route path={'/jeans'} component={Jeans}/>
