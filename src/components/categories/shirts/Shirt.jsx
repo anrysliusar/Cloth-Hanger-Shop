3a946b9624f5cf3b44dbe200615f8d7246c3fb79
andri <assliusarenko@gmail.com>
2021-01-06 23:14:09 +0200

    add component to display all products of category item(shirt)

--- src/components/categories/shirts/Shirt.jsx
+++ src/components/categories/shirts/Shirt.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import classes from './Shirt.module.css'
+import {NavLink, withRouter} from "react-router-dom";
 
-const Shirt = () =>{
-    return <div className={classes.content}>
-        <h3 className={classes.title}>Shirt</h3>
+const Shirt = ({product: product, match: {url} }) =>{
+    return <div>
+        <div>
+            { product.title} - <NavLink to={`${url}/${product.id}`}>details</NavLink>
+        </div>
     </div>
 }
 
-export default Shirt
+export default withRouter(Shirt)
