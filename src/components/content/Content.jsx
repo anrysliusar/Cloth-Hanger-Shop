9cefaa0c488740f11872b61a8b228a9fb74b7a66
andri <assliusarenko@gmail.com>
2021-01-07 22:20:34 +0200

    update content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import classes from './Content.module.css'
 import {Route, Switch} from "react-router-dom";
 import AllCoats from "../categories/coats/AllCoats";
-import Shirt from "../categories/shirts/Shirt";
-import Shoe from "../categories/shoes/Shoe";
-import Jeans from "../categories/jeans/Jeans";
+import AllShoes from "../categories/shoes/AllShoes";
+import AllJeans from "../categories/jeans/AllJeans";
+import AllShirts from "../categories/shirts/AllShirts";
 
 const Content = () =>{
     return <div className={classes.content}>
         <Switch>
             <Route path={'/coats'} component={AllCoats}/>
-            <Route path={'/shirts'} component={Shirt}/>
-            <Route path={'/shoes'} component={Shoe}/>
-            <Route path={'/jeans'} component={Jeans}/>
+            <Route path={'/shirts'} component={AllShirts}/>
+            <Route path={'/shoes'} component={AllShoes}/>
+            <Route path={'/jeans'} component={AllJeans}/>
         </Switch>
     </div>
 }
