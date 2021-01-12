9aad2ca145ddcc59b196514a7c24ed6bf515a2bb
andri <assliusarenko@gmail.com>
2021-01-12 22:58:46 +0200

    added action-creators

---
+++ src/redux/action-creators/index.js
@@ -0,0 +1,17 @@
+import {ProductService} from "../../services/ProductService";
+import {
+    ADD_PRODUCT_TO_CART,
+    ADD_PRODUCT_TO_WISHLIST,
+    SET_PRODUCTS
+} from "../action-types";
+
+
+export const setProducts = () => async (dispatch) => {
+    const productService = new ProductService()
+    const request = await productService.getProducts()
+    const json = await request.json()
+    dispatch({type: SET_PRODUCTS, payload: json})
+}
+
+export const addProductWishlist = (payload) => ({type: ADD_PRODUCT_TO_WISHLIST, payload})
+export const addProductCart = (payload) => ({type: ADD_PRODUCT_TO_CART, payload})
