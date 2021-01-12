fd90b9352e4a50f7d8997a0626f39c9eaeddad0d
andri <assliusarenko@gmail.com>
2021-01-12 23:01:15 +0200

    fix service

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,15 +1,9 @@
-class ProductService {
+export class ProductService {
     url = 'https://fakestoreapi.com'
 
-    findProductById(id){
-        return fetch(`${this.url}/products/${id}`)
-            .then(value => value.json())
-    }
-
-    async products() {
+    async getProducts() {
         return fetch(`${this.url}/products`)
-            .then(value => value.json())
+
     }
 }
 
-export default ProductService
