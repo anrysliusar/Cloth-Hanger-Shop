144dbcccde89606158641d32cbaf8050fb27c5c5
andri <assliusarenko@gmail.com>
2021-01-15 21:59:20 +0200

    added rendering of category components

--- src/components/categories/jeans/AllJeans.jsx
+++ src/components/categories/jeans/AllJeans.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ProductItem from "../../productItem/ProductItem";
+import {useDispatch, useSelector} from "react-redux";
+import {setProducts} from "../../../redux/action-creators";
+import classes from "../../productItem/AllProducts.module.css";
 
 
 const AllJeans = () => {
+    const products = useSelector(({products: {products}}) => products)
+    const dispatch = useDispatch()
+    console.log(products);
+
+    useEffect(() => {
+        dispatch(setProducts())
+    }, [dispatch])
+
     return (
-            <div>
-                <h3>Jeans</h3>
-                <hr/>
+        <div>
+            <h3>Jeans</h3>
+            <hr/>
+            <div className={classes.listItems}>
+                {
+                    products.map(product =>
+                        <ProductItem
+                            product={product}
+                            key={product.id}
+                        />)
+                }
             </div>
-        );
+        </div>
+    );
 }
 
 export default AllJeans
--- src/components/categories/shoes/AllShoes.jsx
+++ src/components/categories/shoes/AllShoes.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ProductItem from "../../productItem/ProductItem";
+import {useDispatch, useSelector} from "react-redux";
+import {setProducts} from "../../../redux/action-creators";
+import classes from "../../productItem/AllProducts.module.css";
 
 
 const AllShoes = () => {
+    const products = useSelector(({products: {products}}) => products)
+    const dispatch = useDispatch()
+    console.log(products);
+
+    useEffect(() => {
+        dispatch(setProducts())
+    }, [dispatch])
+
     return (
         <div>
             <h3>Shoes</h3>
             <hr/>
+            <div className={classes.listItems}>
+                {
+                    products.map(product =>
+                        <ProductItem
+                            product={product}
+                            key={product.id}
+                        />)
+                }
+            </div>
         </div>
     );
 }
